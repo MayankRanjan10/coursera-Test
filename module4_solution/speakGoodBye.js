7ffae0b07822e5bba4bae142e5eb9ffc7d6e0f74
Mayank Ranjan <mayank6027ranjan@gmail.com>
2020-04-06 03:56:39 +0530

    Solution of Assignment4

---
+++ module4_solution/speakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window){
+	var byespeaker={};
+	var speakWord="Goodbye";
+	byespeaker.speak = function(name){
+		console.log(speakeword+" "+byespeaker.name);
+	}
+	window.byespeaker=byespeaker;
+})(window);
---
+++ module4_solution/speakhello.js
@@ -0,0 +1,8 @@
+(function (window){
+	var helloSpeaker = {};
+	var speakWord = "Hello";
+	hellospeaker.speak=function (name){
+		console.log(speakWord+ " "+ hellospeaker.name);
+	}
+	window.hellospeaker=hellospeaker;
+})(window);
